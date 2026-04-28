b099443e1975af9149515b636caaaac5bff99818
parakh-stuvio001 <parakh@stuvio.co>
2026-04-28 14:07:41 +0530

    feat: implement HealthSolutionsCard component with scrollable carousel and animated GSAP card transitions

--- components/CardItemMedium.tsx
+++ components/CardItemMedium.tsx
@@ -6,7 +6,7 @@ import { CarouselItem } from './HealthSolutionsCard';
 const CardItemMedium = ({ item, index, isFixedGrid, cardsColor }: { item: CarouselItem, index: number, isFixedGrid: boolean, cardsColor?: string }) => {
   return (
     <Link
-      href={item.href}
+      href={item.href || "#"}
       className={`group shrink-0 aspect-278/370 [@media(min-width:1920px)]:aspect-270/350 ${cardsColor ? `bg-${cardsColor}` : "bg-white"} flex [@media(min-width:1241px)]:hidden flex-col justify-between relative overflow-hidden px-3.5 sm:px-5 lg:px-6 pt-6 pb-10 gap-y-4 @4xl:gap-y-6 ${isFixedGrid ? "w-75 sm:w-87.5 @6xl:w-full" : "w-[86vw] sm:w-[45vw] lg:w-[40vw] @6xl:w-[calc(33vw-46px)]"}`}
     >
       <div className="flex flex-col w-full items-start mb-4 xl:mb-6 relative z-10">

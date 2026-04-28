ec7499b3503719b35bfea1319596964daebfa474
parakh-stuvio001 <parakh@stuvio.co>
2026-04-28 17:38:49 +0530

    feat: add Step component for displaying the 8-step innovation cycle graphics

--- app/cdmo/Step.tsx
+++ app/cdmo/Step.tsx
@@ -5,7 +5,7 @@ const Step: React.FC = () => {
   return (
     <section className='@container h-fit w-full  relative overflow-hidden py-15 lg:py-20 bg-backgroundSecondary'>
       <SectionHeader heading="8-step Innovation Cycle" />
-      <img src="/images/cdmo/8-step-innovation.svg" alt="8 Step" className='hidden @4xl:block @4xl:max-w-[800px] lg:max-w-[1024px] xl:max-w-[1440px] mx-auto h-auto object-contain object-center px-4 sm:px-6 lg:px-10' />
+      <img src="/images/cdmo/8-step-innovation.svg" alt="8 Step" className='hidden @4xl:block md:max-w-[700px] lg:max-w-[1024px] xl:max-w-[1440px] mx-auto h-auto object-contain object-center px-4 sm:px-6 lg:px-10' />
       <img src="/images/cdmo/8-step-innovation-mobile.svg" alt="8 Step" className='block @4xl:hidden max-w-90 md:max-w-[40%] mx-auto h-auto object-contain object-center px-4 sm:px-6 lg:px-10' />
     </section>
   )
